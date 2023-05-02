0b71380d0ef1707645c11150abe53f3bf1034f5c
fattahsamit <fattahsamit@gmail.com>
2023-05-03 01:40:26 +0600

    activelink, errorpage

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,16 +1,26 @@
 import { useRouteError } from "react-router-dom";
+import ActiveLink from "./components/ActiveLink/ActiveLink";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div id="error-page" className="hero min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold text-error">{error.status}</h1>
+          <p className="py-6 text-warning font-medium">
+            Sorry, an unexpected error has occurred.
+          </p>
+          <p className="mb-6 font-medium">
+            <i>{error.statusText || error.message}</i>
+          </p>
+          <ActiveLink to={"/"}>
+            <button className="btn btn-primary text-white">Get Back</button>
+          </ActiveLink>
+        </div>
+      </div>
     </div>
   );
 }
