ed73dbf78b3660736044fc23235d8cd9fc932577
fattahsamit <fattahsamit@gmail.com>
2023-05-03 00:20:41 +0600

    initial navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
 const Header = () => {
-  return <div>header</div>;
+  return (
+    <nav className="navbar bg-base-100 container mx-auto">
+      <div className="navbar-start">
+        <a className="btn btn-ghost normal-case text-xl">Fattah Samit</a>
+      </div>
+
+      <div className="navbar-end">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a>Homepage</a>
+            </li>
+            <li>
+              <a>Portfolio</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
 
 const Main = () => {
   return (
     <>
-      <h2>Header</h2>
+      <Header></Header>
       <Outlet></Outlet>
-      <h2>Footer</h2>
+      {/* <h2>Footer</h2> */}
     </>
   );
 };
